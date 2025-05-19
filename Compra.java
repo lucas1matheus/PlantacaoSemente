4e3b295aabced2a7a36c61205189f57bc0d5df06
ZardsMK <161655537+ZardsMK@users.noreply.github.com>
2025-05-19 17:30:14 -0300

    Conteudo inserido

--- Compra.java
+++ Compra.java
@@ -1,1 +1,38 @@
-
+public class Compra {
+    private int vl_Orcamento;
+    private int vl_Receita;
+    private int vl_Estoque;
+    private int vl_Venda;
+    private int qt_Semente;
+    
+    public Compra() {
+        
+    }
+    
+    public void comprarSemente(int val, int qtd, Semente sem) {
+        gerarReceita(val, qtd);
+        gerarOrcamento(val, qtd);
+        pegarSemente(qtd, sem);
+    }
+    
+    public void gerarOrcamento(int val, int qtd) {
+        vl_Orcamento = val * qtd;
+        System.out.println("Orçamento gerado: R$ " + vl_Orcamento);
+    }
+    
+    public void gerarReceita(int val, int qtd) {
+        vl_Receita = val * qtd;
+        System.out.println("Receita gerada: R$ " + vl_Receita);
+    }
+    
+    public void pegarSemente(int qtd, Semente sem) {
+        atualizarVenda();
+        atualizarEstoque();
+    }
+    public void atualizarVenda() {
+        
+    }
+    public void atualizarEstoque() {
+        
+    }
+}
