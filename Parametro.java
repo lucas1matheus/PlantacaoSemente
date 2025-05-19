4ac957c5f361473e929e4801f1c72b5d66438ec9
ZardsMK <161655537+ZardsMK@users.noreply.github.com>
2025-05-19 17:30:43 -0300

    Conteudo inserido

--- Parametro.java
+++ Parametro.java
@@ -1,1 +1,21 @@
-
+public class Parametro {
+    private int sementePilula;
+    
+    public Parametro() {
+        sementePilula = 0;
+    }
+    
+    public void atualizarParametro(int val) {
+        int calc = val/30000;
+        if(calc >= 1) {
+            sementePilula = sementePilula + calc;
+            System.out.print(calc + " semente(s) adicionada.");
+        } else {
+            System.out.print("Nenhuma semente adicionada.");
+        }
+    }
+    
+    public int pegarSemente() {
+        return sementePilula;
+    }
+}
