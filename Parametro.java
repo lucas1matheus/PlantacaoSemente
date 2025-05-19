d7158cafe6ab85a313205f79ea2c1af8cb73b94f
lucas1matheus <161655700+lucas1matheus@users.noreply.github.com>
2025-05-19 19:55:57 -0300

    Update Parametro.java

--- Parametro.java
+++ Parametro.java
@@ -1,21 +1,6 @@
 public class Parametro {
-    private int sementePilula;
+    protected int Parametro = 30000;
     
-    public Parametro() {
-        sementePilula = 0;
-    }
-    sdsadsdsa
-    public void atualizarParametro(int val) {
-        int calc = val/30000;
-        if(calc >= 1) {
-            sementePilula = sementePilula + calc;
-            System.out.print(calc + " semente(s) adicionada.");
-        } else {
-            System.out.print("Nenhuma semente adicionada.");
-        }
-    }
-    
-    public int pegarSemente() {
-        return sementePilula;
-    }
+    public void atualizarParametro(int novoParametro) {
+        Parametro = novoParametro;
 }
